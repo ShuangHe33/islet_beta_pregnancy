21059e41891ad5f4430026d6c4a7c7b9e5ecea39
SHUANG HE <41940843+ShuangHe33@users.noreply.github.com>
2025-01-23 17:33:22 +0800

    Update read.10x.rawdata.R

--- 10x_scRNAseq/read.10x.rawdata.R
+++ 10x_scRNAseq/read.10x.rawdata.R
@@ -7,22 +7,14 @@ library(Seurat)
 #                          load
 ###################################################################
 prefix.dir <- 'H:/serve_backup/project/pregnant_mouse/10x_v3/'
-src.raw.Ctrl <- Read10X(data.dir = paste(prefix.dir,"Ctrl_20200416/outs/raw_feature_bc_matrix/",sep = ""))
-src.raw.G14.5_1st <- Read10X(data.dir = paste(prefix.dir,"G14.5_1st_20200416/outs/raw_feature_bc_matrix/",sep = ""))
-src.raw.G14.5_2nd <- Read10X(data.dir = "H:/FileHistory/shuan/DESKTOP-S56EULO/Data/G/project/pregnant_mouse/10x/rawdata/20200308/G14.5_2nd/")
+src.raw.G0 <- Read10X(data.dir = paste(prefix.dir,"G0_20200416/outs/raw_feature_bc_matrix/",sep = ""))
+src.raw.G14.5 <- Read10X(data.dir = paste(prefix.dir,"G14.5_20200416/outs/raw_feature_bc_matrix/",sep = ""))
 
-colnames(src.raw.Ctrl) <- paste0("Ctrl_20200416",colnames(src.raw.Ctrl))
-colnames(src.raw.G14.5_1st) <- paste0("G14.5_1st_20200416",colnames(src.raw.G14.5_1st))
-colnames(src.raw.G14.5_2nd) <- paste0("G14.5_2nd_20200308",colnames(src.raw.G14.5_2nd))
 
-save.image('rawdata/read.10x.CtrlG14.5_1stG14.5_2nd.RData')
-# src.raw.Ctrl=CreateSeuratObject(src.raw.Ctrl)
-# src.raw.G14.5_1st=CreateSeuratObject(src.raw.G14.5_1st)
-# src.raw.G14.5_2nd=CreateSeuratObject(src.raw.G14.5_2nd)
+colnames(src.raw.G0) <- paste0("G0_20200416",colnames(src.raw.G0))
+colnames(src.raw.G14.5) <- paste0("G14.5_20200416",colnames(src.raw.G14.5))
 
-# src.raw.Ctrl$Time="Ctrl"
-# src.raw.G14.5_1st$Time="G14.5_1st"
-# src.raw.G14.5_2nd$Time="G14.5_2nd"
-# src.raw.Ctrl$FACS="Sox9GFP"
-# src.raw.G14.5_1st$FACS="Sox9GFP"
-# src.raw.G14.5_2nd$FACS="Sox9GFP"
+
+save.image('rawdata/read.10x.G0G14.5G14.5_2nd.RData')
+
+
