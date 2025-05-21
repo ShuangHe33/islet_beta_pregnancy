bdf3becf83c84663546f826c13d161ae3ca52cbe
SHUANG HE <41940843+ShuangHe33@users.noreply.github.com>
2025-05-21 10:27:42 +0800

    Update WTG0G14.5.H3K27ac.R

--- chromatin/WTG0G14.5.H3K27ac.R
+++ chromatin/WTG0G14.5.H3K27ac.R
@@ -198,26 +198,7 @@ boxplot(as.data.frame(G14.5HFD.bed)[,paste(prefix.list$G14.5HFD.H3K27ac,'.nor',s
 boxplot(as.data.frame(G14.5HFD.bed)[,paste(c('G0HFD','G14.5CD','G14.5HFD'),his,'nor.mean',sep = '.')],outline=F,notch=T)
 par(oma= c(8,0,0,0))
 MyHeatmap(as.data.frame(G14.5HFD.bed)[,paste(prefix.list$G14.5HFD.H3K27ac[c(1:2,6:7,3:5)],'.nor',sep = '')],Colv = 'none')
-# boxplot(as.data.frame(G14.5HFD.bed)[,c('G14.5CD.G14.5CDHFD.nor.mean','G14.5HFD.G14.5CDHFD.nor.mean','WT.Acss2HFD.nor.mean','HMKO.Acss2HFD.nor.mean')],outline=F)
-# boxplot(as.data.frame(G14.5HFD.bed[G14.5HFD.bed$promoter2==1])[,c('G14.5CD.G14.5CDHFD.nor.mean','G14.5HFD.G14.5CDHFD.nor.mean')],outline=F,notch=T,main = 'promoter')
-# boxplot(as.data.frame(G14.5HFD.bed[G14.5HFD.bed$enhancer==1])[,c('G14.5CD.G14.5CDHFD.nor.mean','G14.5HFD.G14.5CDHFD.nor.mean')],outline=F,notch=T,main = 'enhancer')
-# wilcox.test.res1 <- wilcox.test(as.data.frame(G14.5HFD.bed[G14.5HFD.bed$enhancer==1])[,c('G14.5CD.G14.5CDHFD.nor.mean')],
-#                                 as.data.frame(G14.5HFD.bed[G14.5HFD.bed$enhancer==1])[,c('G14.5HFD.G14.5CDHFD.nor.mean')],
-#                                 alternative = 'less',
-#                                 paired = T)
-# 
-# MyText(paste("p-value: \n",
-#              wilcox.test.res1$p.value,"\n"),
-#        text.cex = 1)
-# 
-# 
-# gene.up.venn <- Venn(list('H3K27ac up' = unique(G14.5HFD.up.bed$gene.id),
-#                           'G14.5HFD up' = G14.5HFD.up.gene
-# ))
-# 
-# plot(gene.up.venn)
-# rownames(genes.inf.input) <- genes.inf.input$EnsemblGeneID
-# genes.inf.input[gene.up.venn@IntersectionSets$`11`,'Symbol']
+
 ########
 merge.bed <- merge.bed[merge.bed@ranges@width>=500]
 G14.5HFD.bed <- merge.bed[merge.bed$G14.5HFD=='up']
@@ -229,37 +210,7 @@ merge.bed$G14.5HMKOHFD.nor.mean <- round((merge.bed$Ins1G14.5Acss2HMKO_HFD_7_H3K
 
 boxplot(as.data.frame(merge.bed)[,paste(c('G0HFD','G14.5CD','G14.5HFD','G14.5HMKOHFD'),'nor.mean',sep = '.')],outline=F,notch=T)
 
-# G14.5HFD.gene <- MyReadDelim('G:/project/pregnant_mouse/beta/sm3/ref/new_sm3/ref_final/HFD/KO/20220913/G0G14.5HMKO/addG18.5/final/overlap/gene_inf/v2.c1_HFDgene.tab')
-# G14.5HFD.down.gene <- MyReadDelim('G:/project/pregnant_mouse/beta/sm3/ref/new_sm3/ref_final/HFD/KO/20220913/G0G14.5HMKO/addG18.5/final/overlap/gene_inf/v2.c2_HMHFDgene.tab')
-# 
-# 
-# G14.5HFD.c1.bed <- merge.bed[merge.bed$gene.id %in% G14.5HFD.gene$EnsemblGeneID]
-# G14.5HFD.c2.bed <- merge.bed[merge.bed$gene.id %in% G14.5HFD.down.gene$EnsemblGeneID]
-# boxplot(as.data.frame(G14.5HFD.c1.bed)[,paste(prefix.list$G14.5WTHFD.H3K27ac,'nor',sep = '.')],outline=F)
-# boxplot(as.data.frame(merge.bed)[,paste(prefix.list$G14.5WTHFD.H3K27ac,'nor',sep = '.')],outline=F)
-# 
-# 
-# pdf('Figure/CDHFDHMKO.gene.signal.intensity.pdf',3,5)
-# MyLogBoxPlot(as.data.frame(G14.5HFD.c1.bed)[,c('CD.G14.5CDHFD.H3K27ac.nor.mean','WT.G14.5CDHFD.H3K27ac.nor.mean','HMKO.G14.5CDHFD.H3K27ac.nor.mean')],log = F,
-#              min.cutoff = 0,notch=T,col=add.alpha(time.colors[c(8,13,26)],0.8),lwd=4,ylab='signal intensity', main = 'WT',ylim = c(0,20)
-# )
-# box(lwd=3)
-# # WT.p <- wilcox.test(as.data.frame(G14.5HFD.c1.bed)[,c('CD.G14.5CDHFD.H3K27ac.nor.mean')],
-# #                     as.data.frame(G14.5HFD.c1.bed)[,c('WT.G14.5CDHFD.H3K27ac.nor.mean')],
-# #                     pair=T)
-# # MyText(paste("CDHFD:p-value: \n",
-# #              WT.p$p.value,"\n"),
-# #        text.cex = 0.6)
-# 
-# WT.p <- wilcox.test(as.data.frame(G14.5HFD.c1.bed)[,c('HMKO.G14.5CDHFD.H3K27ac.nor.mean')],
-#                     as.data.frame(G14.5HFD.c1.bed)[,c('WT.G14.5CDHFD.H3K27ac.nor.mean')],
-#                     alternative = 'less',
-#                     pair=T)
-# MyText(paste("WT_HMKO:p-value: \n",
-#              WT.p$p.value,"\n"),
-#        text.cex = 0.6)
-# 
-# dev.off()
+
 ##########cutoff######
 split.bed.list <- list()
 split.bed.list[["G14.5HFD.H3K27ac"]] <- list.G14.5Acss2CDHFD.consensuspeak[c("G0.c1","G14.5CD.c1",'c1')]
@@ -530,108 +481,7 @@ MyText(paste("G14.5WT KOp-value: \n",
              P300.p$p.value,"\n"),
        text.cex = 0.5)
 dev.off()
-########
-# rm(list.Acss2HFD.H3K27ac.bdg)
-# save.image('H3K27ac.RData')
-# #######up down fold ahnge####
-# H3K27ac.fc <- 1.5
-# pdf('Figure/edage/G18.5Stat3WT.fc1.5.pdf',6,6.5)
-# merge.bed <- MyfcDataplot2(merge.bed,
-#                            "G14.5CD.G14.5CDHFD.nor.mean",
-#                            "G14.5HFD.G14.5CDHFD.nor.mean",
-#                            "G14.5CDHFD.H3K27ac.fc2.state",
-#                            "G14.5CDHFD.H3K27ac.fc2.up",
-#                            "G14.5CDHFD.H3K27ac.fc2.down",
-#                            H3K27ac.fc,
-#                            cutoff.list[[type]],
-#                            c(-10,8),
-#                            c(-10,8)
-# )
-# dev.off()
-# #####
-# prefix.list$G14.5CDHFD.H3K27ac <- prefix.list$G14.5CDHFD.H3K27ac[c(1:4,6:9)]
-# 
-# G14.5HFD.up.bed <- merge.bed[merge.bed$G14.5CDHFD.H3K27ac.fc2.state=='up']
-# G14.5HFD.up.bed <- merge.bed[merge.bed$G14.5CDHFD.H3K27ac.fc2.state=='up']
-# G14.5HFD.up.bed <- merge.bed[G14.5HFD.up]
-# table(G14.5HFD.up.bed$G14.5HFD.G14.5CDHFD.nor.state)
-# G14.5HFD.up.bed <- G14.5HFD.up.bed[G14.5HFD.up.bed$G14.5HFD.G14.5CDHFD.nor.state==1]
-# table(G14.5HFD.up.bed$promoter2)
-# 
-# boxplot(as.data.frame(G14.5HFD.up.bed)[,paste(prefix.list$G14.5HFD.H3K27ac,'.nor',sep = '')],outline=T)
-# 
-# 
-# boxplot(as.data.frame(G14.5HFD.up.bed)[,c('G0HFD.nor.mean','G14.5CD.nor.mean','G14.5HFD.nor.mean','G14.5HMKOHFD.nor.mean')],outline=F,
-#         notch=T,main = 'H3K27ac',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:4,labels = c('G14.5CD','G14.5HFD','G14.5Acss2WTHFD','G14.5Acss2HMKOHFD'),cex.axis = 1)
-# 
-# p.val <- wilcox.test(as.data.frame(G14.5HFD.up.bed)[,c('G14.5HFD.nor.mean')],
-#                      as.data.frame(G14.5HFD.up.bed)[,c('G14.5HMKOHFD.nor.mean')],
-#                      alternative = 'greater',
-#                      paired = T
-# )
-# p.val$p.value
-# 
-# 
-# boxplot(as.data.frame(G14.5HFD.up.bed)[,c('G14.5CD.G14.5CDHFD.H3K4me1.nor.mean','G14.5HFD.G14.5CDHFD.H3K4me1.nor.mean')],outline=F,
-#         notch=T,main = 'H3K4me1',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:2,labels = c('G14.5CD','G14.5HFD'),cex.axis = 1)
-# 
-# boxplot(as.data.frame(G14.5HFD.up.bed)[,c('G14.5CD.G14.5CDHFD.ATAC.mean','G14.5HFD.G14.5CDHFD.ATAC.mean')],outline=F,
-#         notch=T,main = 'ATAC',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:2,labels = c('G14.5CD','G14.5HFD'),cex.axis = 1)
-# 
-# G14.5HFD.down.bed <- merge.bed[merge.bed$G14.5CDHFD.H3K27ac.fc2.state=='down']
-# table(G14.5HFD.down.bed$promoter2)
-# 
-# G14.5HFD.down.bed <- merge.bed[G14.5HFD.down]
-# table(G14.5HFD.down.bed$G14.5CD.G14.5CDHFD.nor.state)
-# G14.5HFD.down.bed <- G14.5HFD.down.bed[G14.5HFD.down.bed$G14.5CD.G14.5CDHFD.nor.state==1]
-# table(G14.5HFD.down.bed$promoter2)
-# table(G14.5HFD.up.bed$promoter2)
-# 
-# boxplot(as.data.frame(G14.5HFD.down.bed)[,paste(prefix.list$G14.5CDHFD.H3K27ac,'.nor',sep = '')],outline=F)
-# 
-# boxplot(as.data.frame(G14.5HFD.down.bed)[,c('G14.5CD.G14.5CDHFD.nor.mean','G14.5HFD.G14.5CDHFD.nor.mean','WT.Acss2HFD.nor.mean','HMKO.Acss2HFD.nor.mean')],outline=F,
-#         notch=T,main = 'H3K27ac',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:4,labels = c('G14.5CD','G14.5HFD','G14.5Acss2WTHFD','G14.5Acss2HMKOHFD'),cex.axis = 1)
-# 
-# boxplot(as.data.frame(G14.5HFD.down.bed)[,c('G14.5CD.G14.5CDHFD.H3K4me1.nor.mean','G14.5HFD.G14.5CDHFD.H3K4me1.nor.mean')],outline=F,
-#         notch=T,main = 'H3K4me1',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:2,labels = c('G14.5CD','G14.5HFD'),cex.axis = 1)
-# 
-# boxplot(as.data.frame(G14.5HFD.down.bed)[,c('G14.5CD.G14.5CDHFD.ATAC.mean','G14.5HFD.G14.5CDHFD.ATAC.mean')],outline=F,
-#         notch=T,main = 'ATAC',xaxt="n",ylab = 'signal intensity'
-# )
-# axis(1,at = 1:2,labels = c('G14.5CD','G14.5HFD'),cex.axis = 1)
-# 
-# 
-# G14.5HFD.down.p.bed <- G14.5HFD.down.bed[G14.5HFD.down.bed$promoter2==1]
-# G14.5HFD.down.e.bed <- G14.5HFD.down.bed[G14.5HFD.down.bed$enhancer==1]
-# G14.5HFD.up.p.bed <- G14.5HFD.up.bed[G14.5HFD.up.bed$promoter2==1]
-# G14.5HFD.up.e.bed <- G14.5HFD.up.bed[G14.5HFD.up.bed$enhancer==1]
-# 
-# list.H3K27ac.element.bed[['G14.5HFD.down.e']] <- G14.5HFD.down.e.bed
-# list.H3K27ac.element.bed[['G14.5HFD.up.e']] <- G14.5HFD.up.e.bed
-# 
-# #######go kegg#######
-# list.H3K27ac.element.bed[['G14.5HFD.up']] <- G14.5HFD.up.bed
-# list.H3K27ac.element.bed[['G14.5HFD.down']] <- G14.5HFD.down.bed
-# dir.create('go_kegg')
-# dir.create('bed')
-# 
-# for(tmp.cluster in names(list.H3K27ac.element.bed)){
-#   MyGOwritetable(list.H3K27ac.element.bed[[tmp.cluster]]$gene.id,pvalue = 1,paste('go_kegg/go.',tmp.cluster,'.',length(list.H3K27ac.element.bed[[tmp.cluster]]),'.tab',sep = ''))
-# }
-# 
-# for(tmp.cluster in names(list.H3K27ac.element.bed)[9:10]){
-#   MyWriteBed(list.H3K27ac.element.bed[[tmp.cluster]],paste('bed/',tmp.cluster,'.',length(list.H3K27ac.element.bed[[tmp.cluster]]),'.bed',sep = ''))
-# }
+
 ###########
 rm(list.Acss2HFD.H3K27ac.bdg)
 saveRDS(list.close,'list.close.rds')
